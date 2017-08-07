02b1d8a172b9ad599c1f08142af2cbff07626f56
genzzhang <genzzhang@163.com>
2017-08-07 16:00:43 +0800

    rich text

--- Demo/app/src/main/java/com/genzzhang/demo/richtexts/RichTextsActivity.java
+++ Demo/app/src/main/java/com/genzzhang/demo/richtexts/RichTextsActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -32,6 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RichTextsActivity extends AbsActivity {
+    private final static String TAG = "RichTextsActivity";
     // 描述黄色背景
     private Drawable mDescriBg;
     private ArrayList<Integer> mDescriSize = new ArrayList<Integer>(5);
@@ -43,17 +45,13 @@ public class RichTextsActivity extends AbsActivity {
 
         //描述设置
         mDescriBg = getResources().getDrawable(R.drawable.rich_texts_mark_yellow_bg);
-        mDescriSize.add(Tools.dip2px(12));//字体大小
+        mDescriSize.add(Tools.dip2px(8));//字体大小
         mDescriSize.add(Tools.dip2px(2));//增加水平间距
         mDescriSize.add(Tools.dip2px(1));//背景边距设置的1dp mMarkBg
-        Paint paint = new Paint();
-        paint.setTextSize(mDescriSize.get(0));
-        Paint.FontMetricsInt fMetrics = paint.getFontMetricsInt();
-        mDescriSize.add(fMetrics.bottom + fMetrics.top); //字体
-        mDescriSize.add(fMetrics.descent - fMetrics.ascent); //字体净高度
 
-        TextView textView = (TextView) findViewById(R.id.title);
-        textView.setText(getSpannableTitle("解决文本绘制不居中的问题", "文TextView本"));
+        ((TextView) findViewById(R.id.title)).setText(
+                getSpannableTitle("设置行距、边距、行间距倍数等，保持居中显示问题", "文本"));
+
     }
 
     private SpannableString getSpannableTitle(String title, String mark) {
@@ -65,42 +63,47 @@ public class RichTextsActivity extends AbsActivity {
         final int fontPadding = mDescriSize.get(1);
         //背景边距设置的1dp mMarkBg
         final int cornerSize = mDescriSize.get(2);
-        SpannableString msp = new SpannableString(title + mark + "谣言不转不是中国人");
+        SpannableString msp = new SpannableString(title + mark);
         msp.setSpan(new ImageSpan(mDescriBg) {
             @Override
             public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
                 paint.setTextSize(fontSize);
                 width.add(0, Math.round(paint.measureText(text, start, end)));
-                //整体偏移2个fontPadding 加上文字左右各一个，一共4
-                return width.get(0) + fontPadding * 4;
+                //加上文字左右各一个
+                return width.get(0) + 2 * fontPadding;
             }
             @Override
             public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom,
                              Paint paint) {
-                //绘制背景图
+                canvas.drawLine(x, top, x + 300, top, paint);
+                canvas.drawLine(x, y, x + 300, y, paint);
+                canvas.drawLine(x, bottom, x + 300, bottom, paint);
+
+                float oldad = paint.descent() + paint.ascent();
                 paint.setTextSize(fontSize);
-                int textHeight = mDescriSize.get(4);
-                int oldHeight = bottom - top;
+                float newad = paint.descent() + paint.ascent();
+                //以改变前的为中间基准
+                int baseY = (int)(y * 2 + oldad - newad ) / 2;
+                int textHeight = (int)(paint.descent() - paint.ascent());
                 //字体净高度 + 背景边界宽度=总高度
                 int height = textHeight + cornerSize * 2;
-                int dp = (int)(oldHeight - height) / 2;
+                //以新的文字中心点基准
+                int bgBottom = (int)((2 * baseY + newad + height) / 2.0);
                 getDrawable().setBounds(
                         0,
                         0,
-                        width.get(0) + fontPadding * 2,
+                        width.get(0) + 2 * fontPadding,
                         height);
-                super.draw(canvas, text, start, end, x + 2 * fontPadding, top, y, bottom - dp, paint);
-                // 绘制文本
-                //中文字符绘制的中间位置的基准点一般设置在（总体高度/2 - 文字高度/2）
-                //位置 http://blog.csdn.net/hursing/article/details/18703599
-                int baseY = (int)(top + bottom - mDescriSize.get(3)) / 2;
+                //绘制背景图
+                super.draw(canvas, text, start, end, x, top, y, bgBottom, paint);
                 paint.setColor(Color.RED);
                 //paint.setTypeface(Typeface.create("normal", Typeface.BOLD));
                 canvas.drawText(text.subSequence(start, end).toString(),
-                        x + fontPadding * 3,
+                        x + fontPadding,
                         baseY, paint);
             }
         }, title.length(), title.length() + mark.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
         return msp;
     }
 }
